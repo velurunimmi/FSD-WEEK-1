80999aeda9cb06ad390614a862b172880822bfb6
velurunimmi <velurunimmi@gmail.com>
2026-06-22 20:33:03 +0530

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,10 @@
+script2.js
+function selectElements()
+{
+let titlejs = document.getElementById("title");
+titlejs.style.color = "blue";
+let messagejs = document.getElementsByClassName("message")[0];
+messagejs.style.color = "green";
+let paragraphs = document.getElementsByTagName("p");
+paragraphs[1].style.backgroundColor = "yellow";
+}
