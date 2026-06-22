390d1077a282c6b1608066ddd2be4eaef8dc1da9
velurunimmi <velurunimmi@gmail.com>
2026-06-22 20:56:33 +0530

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,9 @@
+script3.js
+document.addEventListener("DOMContentLoaded", function () {
+let button = document.getElementById("myButton");
+button.addEventListener("click", function () {
+let text = document.getElementById("text");
+text.textContent = "The text has been changed!";
+text.style.color = "green";
+});
+});
